eafc4bbb3f817cff3e8ae3ab491cc58e77a6aeed
ahmed101-uni <a.khalid@alustudent.com>
2025-02-21 15:46:25 -0600

    task1

---
+++ api_advanced/1-top_ten.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+THe module prints the titles of the top
+10 hot posts from a subreddit.
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """
+    The function fetches and prints the titles
+    of the top 10 hot posts from a subreddit.
+    """
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(
+        url,
+        params={"after": None},
+        allow_redirects=False,
+        headers=headers,
+    )
+
+    if response.status_code != 200:
+        print(None)
+        return
+
+    jsonData = response.json()
+    data = jsonData["data"]["children"]
+    for post in data:
+        print(post.get("data", {}).get("title"))
+
+
+
